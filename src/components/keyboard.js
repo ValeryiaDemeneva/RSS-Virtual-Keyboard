a2eb2c8303f7a84e5efd9d34e92063c8de26b20b
v.dddemeneva <v.dddemeneva@gmail.com>
2022-05-08 14:30:53 +0300

    feat: update localStore items

--- src/components/keyboard.js
+++ src/components/keyboard.js
@@ -1,57 +1,67 @@
 import { ruLang, engLang, ruLangCaps, engLangCaps, keyCode } from "./data";
 
-let language = "ru";
+let language = localStorage.getItem("lang") || "ru";
 let textValue = "";
 const body = document.querySelector("body");
 const container = document.createElement("div");
 body.append(container);
 const textArea = document.createElement("textarea");
 textArea.value = textValue;
-textArea.readOnly = true;
 container.append(textArea);
 
 const onAddClick = (button) => {
   const { innerHTML: text } = button;
-  if (text === "Backspase") {
+  if (text === "Backspace") {
     textValue = textValue.slice(0, -1);
   } else if (text === "Tab") {
     textValue += "   ";
-  } else if (text === "Del") {
+  } else if (text === "Delete") {
+    textValue = " ";
   } else if (text === "CapsLock") {
     if (language === "ru") {
       onRenderKeyboard(ruLangCaps);
       language = "ruCaps";
+      localStorage.setItem("lang", "ruCaps");
     } else if (language === "ruCaps") {
       onRenderKeyboard(ruLang);
       language = "ru";
+      localStorage.setItem("lang", "ru");
     } else if (language === "en") {
       onRenderKeyboard(engLangCaps);
       language = "enCaps";
+      localStorage.setItem("lang", "enCaps");
     } else if (language === "enCaps") {
       language = "en";
       onRenderKeyboard(engLang);
+      localStorage.setItem("lang", "en");
     }
   } else if (text === "Enter") {
     textValue += "\n";
-  } else if (text === "Shift") {
-  } else if (text === "Ctrl") {
-  } else if (text === "Alt") {
+  } else if (text === "SPACE") {
+    textValue += " ";
   } else if (text === "CtrlR") {
+    //HERE
     if (language === "ru") {
       language = "en";
       onRenderKeyboard(engLang);
+      localStorage.setItem("lang", "en");
     } else if (language === "ruCaps") {
       onRenderKeyboard(engLangCaps);
       language = "enCaps";
+      localStorage.setItem("lang", "enCaps");
     } else if (language === "en") {
       onRenderKeyboard(ruLang);
       language = "ru";
+      localStorage.setItem("lang", "ru");
     } else if (language === "enCaps") {
       language = "ruCaps";
       onRenderKeyboard(ruLangCaps);
+      localStorage.setItem("lang", "ruCaps");
     }
   } else {
-    textValue += text;
+    if (text !== "Shift" && text !== "Ctrl" && text !== "Alt") {
+      textValue += text;
+    }
   }
   textArea.value = textValue;
 };
@@ -60,18 +70,23 @@ const onMouseDown = (button) => {
   if (button.innerHTML === "Shift") {
     if (language === "ru") {
       language = "ruCaps";
+      localStorage.setItem("lang", "ruCaps");
+
       onRenderKeyboard(ruLangCaps);
     } else if (language === "ruCaps") {
       language = "ru";
       onRenderKeyboard(ruLang);
+      localStorage.setItem("lang", "ru");
     } else if (language === "en") {
       language = "enCaps";
       onRenderKeyboard(engLangCaps);
+      localStorage.setItem("lang", "enCaps");
     } else if (language === "enCaps") {
       language = "en";
       onRenderKeyboard(engLang);
+      localStorage.setItem("lang", "en");
     }
-    document.querySelector(".Shift").classList.add("active");
+    document.querySelector(".key-Shift").classList.add("active");
   }
 };
 
@@ -80,17 +95,21 @@ const onMouseUp = (button) => {
     if (language === "ruCaps") {
       language = "ru";
       onRenderKeyboard(ruLang);
+      localStorage.setItem("lang", "ru");
     } else if (language === "ru") {
       language = "ruCaps";
       onRenderKeyboard(ruLangCaps);
+      localStorage.setItem("lang", "ruCaps");
     } else if (language === "en") {
       language = "enCaps";
       onRenderKeyboard(engLangCaps);
+      localStorage.setItem("lang", "enCaps");
     } else if (language === "enCaps") {
       language = "en";
       onRenderKeyboard(engLang);
+      localStorage.setItem("lang", "en");
     }
-    document.querySelector(".Shift").classList.remove("active");
+    document.querySelector(".key-Shift").classList.remove("active");
   }
 };
 const onCreateKeyboard = (arr) => {
@@ -108,6 +127,7 @@ const onCreateKeyboard = (arr) => {
       keyboardLine.append(keyboardButton);
       keyboardButton.innerHTML = btn;
       keyboardButton.classList.add(`key-${btn}`);
+      keyboardButton.dataset.key = btn;
       keyboardButton.addEventListener("click", () => {
         onAddClick(keyboardButton);
       });
@@ -125,38 +145,122 @@ const onKeyUpButton = (event) => {
   const { code } = event;
   if (code === "Backspace") {
     textValue = textValue.slice(0, -1);
+    document
+      .querySelector("div[data-key='" + "Backspace" + "']")
+      .classList.remove("active");
   } else if (code === "Tab") {
+    event.preventDefault();
     textValue += "   ";
+    document
+      .querySelector("div[data-key='" + "Tab" + "']")
+      .classList.remove("active");
   } else if (code === "Enter") {
     textValue += "\n";
   } else if (code === "CapsLock") {
     if (language === "ru") {
       onRenderKeyboard(ruLangCaps);
       language = "ruCaps";
+      localStorage.setItem("lang", "ruCaps");
     } else if (language === "ruCaps") {
       onRenderKeyboard(ruLang);
       language = "ru";
+      localStorage.setItem("lang", "ru");
     } else if (language === "en") {
       onRenderKeyboard(engLangCaps);
       language = "enCaps";
+      localStorage.setItem("lang", "enCaps");
     } else if (language === "enCaps") {
       language = "en";
       onRenderKeyboard(engLang);
+      localStorage.setItem("lang", "en");
     }
   } else if (code === "ArrowUp") {
     textValue += "⇑";
+    document
+      .querySelector("div[data-key='" + "⇑" + "']")
+      .classList.remove("active");
   } else if (code === "ArrowDown") {
     textValue += "⇓";
+    document
+      .querySelector("div[data-key='" + "⇓" + "']")
+      .classList.remove("active");
   } else if (code === "ArrowLeft") {
     textValue += "⇐";
+    document
+      .querySelector("div[data-key='" + "⇐" + "']")
+      .classList.remove("active");
+  } else if (code === "ShiftLeft") {
+    if (language === "ruCaps") {
+      language = "ru";
+      onRenderKeyboard(ruLang);
+      localStorage.setItem("lang", "ru");
+    } else if (language === "ru") {
+      language = "ruCaps";
+      onRenderKeyboard(ruLangCaps);
+      localStorage.setItem("lang", "ruCaps");
+    } else if (language === "en") {
+      language = "enCaps";
+      onRenderKeyboard(engLangCaps);
+      localStorage.setItem("lang", "enCaps");
+    } else if (language === "enCaps") {
+      language = "en";
+      onRenderKeyboard(engLang);
+      localStorage.setItem("lang", "en");
+    }
+    document
+      .querySelector("div[data-key='" + "Shift" + "']")
+      .classList.remove("active");
   } else if (code === "ArrowRight") {
     textValue += "⇒";
+    document
+      .querySelector("div[data-key='" + "⇒" + "']")
+      .classList.remove("active");
+  } else if (code === "ControlLeft") {
+    if (language === "ru") {
+      language = "en";
+      onRenderKeyboard(engLang);
+      localStorage.setItem("lang", "en");
+    } else if (language === "ruCaps") {
+      onRenderKeyboard(engLangCaps);
+      language = "enCaps";
+      localStorage.setItem("lang", "enCaps");
+    } else if (language === "en") {
+      onRenderKeyboard(ruLang);
+      language = "ru";
+      localStorage.setItem("lang", "ru");
+    } else if (language === "enCaps") {
+      language = "ruCaps";
+      localStorage.setItem("lang", "ruCaps");
+
+      onRenderKeyboard(ruLangCaps);
+    }
+    document
+      .querySelector("div[data-key='" + "Ctrl" + "']")
+      .classList.remove("active");
+  } else if (code === "ControlRight") {
+    document
+      .querySelector("div[data-key='" + "CtrlR" + "']")
+      .classList.remove("active");
+  } else if (code === "Delete") {
+    document
+      .querySelector("div[data-key='" + "Delete" + "']")
+      .classList.remove("active");
+    textValue = " ";
+  } else if (code === "AltLeft") {
+    event.preventDefault();
+    document
+      .querySelector("div[data-key='" + "Alt" + "']")
+      .classList.remove("active");
+  } else if (code === "Space") {
+    textValue += " ";
+    document
+      .querySelector("div[data-key='" + "SPACE" + "']")
+      .classList.remove("active");
   } else {
     keyCode.forEach((item) => {
       if (item.code === code) {
-        textValue += item[language];
         document
-          .querySelector(`.key-${item[language]}`)
+          .querySelector("div[data-key='" + item[language] + "']")
           .classList.remove("active");
       }
     });
@@ -165,12 +269,70 @@ const onKeyUpButton = (event) => {
 };
 
 const onKeyDownButton = (event) => {
-  keyCode.forEach((item) => {
-    if (item.code === event.code) {
-      textValue += item[language];
-      document.querySelector(`.key-${item[language]}`).classList.add("active");
+  const { code } = event;
+  if (code === "ShiftLeft") {
+    if (language === "ru") {
+      language = "ruCaps";
+      onRenderKeyboard(ruLangCaps);
+      localStorage.setItem("lang", "ruCaps");
+    } else if (language === "ruCaps") {
+      language = "ru";
+      onRenderKeyboard(ruLang);
+      localStorage.setItem("lang", "ru");
+    } else if (language === "en") {
+      language = "enCaps";
+      onRenderKeyboard(engLangCaps);
+      localStorage.setItem("lang", "enCaps");
+    } else if (language === "enCaps") {
+      language = "en";
+      onRenderKeyboard(engLang);
+      localStorage.setItem("lang", "en");
     }
-  });
+    document
+      .querySelector("div[data-key='" + "Shift" + "']")
+      .classList.add("active");
+  } else if (code === "Backspace") {
+    document
+      .querySelector("div[data-key='" + "Backspace" + "']")
+      .classList.add("active");
+  } else if (code === "Space") {
+    textValue += " ";
+    document
+      .querySelector("div[data-key='" + "SPACE" + "']")
+      .classList.add("active");
+  } else if (code === "AltLeft") {
+    event.preventDefault();
+    document
+      .querySelector("div[data-key='" + "Alt" + "']")
+      .classList.add("active");
+  } else if (code === "Tab") {
+    event.preventDefault();
+    document
+      .querySelector("div[data-key='" + "Tab" + "']")
+      .classList.add("active");
+  } else if (code === "ControlLeft") {
+    document
+      .querySelector("div[data-key='" + "Ctrl" + "']")
+      .classList.add("active");
+  } else if (code === "ControlRight") {
+    document
+      .querySelector("div[data-key='" + "CtrlR" + "']")
+      .classList.add("active");
+  } else if (code === "CapsLock") {
+    event.preventDefault();
+    document
+      .querySelector("div[data-key='" + "CapsLock" + "']")
+      .classList.add("active");
+  } else {
+    keyCode.forEach((item) => {
+      if (item.code === code) {
+        textValue += item[language];
+        document
+          .querySelector("div[data-key='" + item[language] + "']")
+          .classList.add("active");
+      }
+    });
+  }
 };
 
 const onAddDocumentListener = () => {
@@ -190,7 +352,28 @@ const onRenderKeyboard = (arr) => {
 
 const init = () => {
   onAddDocumentListener();
-  onCreateKeyboard(ruLang);
+  const savedLang = localStorage.getItem("lang");
+
+  switch (savedLang) {
+    case "ru":
+      onCreateKeyboard(ruLang);
+      break;
+
+    case "ruCaps":
+      onCreateKeyboard(ruLangCaps);
+      break;
+
+    case "en":
+      onCreateKeyboard(engLang);
+      break;
+
+    case "enCaps":
+      onCreateKeyboard(engLangCaps);
+      break;
+
+    default:
+      onCreateKeyboard(ruLang);
+  }
 };
 
 init();
